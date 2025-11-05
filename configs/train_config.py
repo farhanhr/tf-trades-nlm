32fcd3639edd25498bf3b925f5bd87362ba0c7f3
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-11-05 12:58:58 +0700

    update train_config

--- configs/train_config.py
+++ configs/train_config.py
@@ -1,11 +1,11 @@
 config = {
-    "epochs": 100,
+    "epochs": 200,
     "batch_size": 32,
     "momentum": 0.9,
-    "step_size": 1, #pixels
+    "step_size": 3, #pixels
     "epsilon": 4,  #pixels
     "perturb_steps": 10,
-    "beta": 5.0,
+    "beta": 3,
     "val_subset_size": 512,
     "dataset_path": "data/datasets/",
     "saved_models_path": "models/saved_models/",
