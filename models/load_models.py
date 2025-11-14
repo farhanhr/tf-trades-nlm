f862986dd6421268bd070a5cca4b99e62edbc89a
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-11-14 16:34:24 +0700

    update load_model

--- models/load_models.py
+++ models/load_models.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
 
-def load_models(saved_models_path, compile=False):
+def load_models(saved_models_path, compile=False, trainable=False):
     model = tf.keras.models.load_model(saved_models_path, compile=compile)
+    if trainable == True:
+        # Unfreeze 10 last layers
+        for layer in model.layers[-10:]:
+            if isinstance(layer, tf.keras.layers.BatchNormalization):
+                layer.trainable = False
+            else:
+                layer.trainable = True
+                print("[INFO] Unfroze the layers of VGG16.")
     return model
