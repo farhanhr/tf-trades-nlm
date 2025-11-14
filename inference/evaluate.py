3875fe043500bcd02d30c8931d1974d06125734f
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-11-14 16:34:09 +0700

    update evaluate.py

--- inference/evaluate.py
+++ inference/evaluate.py
@@ -1,10 +1,22 @@
-from data.data_loader import get_testing_data
+from sklearn.metrics import classification_report
+import pandas as pd
+import tensorflow as tf
 from models.load_models import load_models
 from configs.train_config import config
 
-test_gen = get_testing_data(config["dataset_path"], config["batch_size"])
-saved_models_path = config["saved_models_path"] + "BrainTumorMRI_VGG16_26102025_0850_model.h5"
-model = load_models(saved_models_path, compile=False)
+def model_classification_report(model_name, test_gen):
+    model_path = config["saved_models_path"] + model_name
+    model = load_models(model_path, compile=True)
+    model.compile(optimizer='adam', loss='SparseCategoricalCrossentropy', metrics=['accuracy'])
+    test_loss, test_acc = model.evaluate(test_gen)
+    print(f"Model Evaluation: {model_name}")
+    print(f"Test Accuracy: {test_acc:.2f}\n Test Loss: {test_loss:.2f} \n")
 
-test_loss, test_acc = model.evaluate(test_gen)
-print(f"Test Accuracy: {test_acc:.2f}")
+    y_pred_probs = model.predict(test_gen)
+    y_pred = y_pred_probs.argmax(axis=1)
+    y_true = test_gen.classes
+
+    class_names = list(test_gen.class_indices.keys())
+    report = classification_report(y_true, y_pred, target_names=class_names, output_dict=True)
+    df_report = pd.DataFrame(report).T
+    print(df_report)
